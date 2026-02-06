462ac54cea0dc26cbf68985338fc1810a07a9703
nub-belajar123 <jessshi082@gmail.com>
2026-02-07 00:30:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function updateRTP() {
 }
 
 setInterval(updateRTP, 2000);
+
+const games = [
+  { id: "game1", value: 96.20 },
+  { id: "game2", value: 97.10 },
+  { id: "game3", value: 95.80 }
+];
+
+function updateGames() {
+  games.forEach(game => {
+    game.value += (Math.random() * 0.3 - 0.15);
+
+    if (game.value < 94) game.value = 94;
+    if (game.value > 98) game.value = 98;
+
+    document.getElementById(game.id).innerText =
+      game.value.toFixed(2) + "%";
+  });
+}
+
+setInterval(updateGames, 3000);
